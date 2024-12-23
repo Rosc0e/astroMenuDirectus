4eb7b5f54b1aa2b339d13412554589d36edb47b9
John Doe <Blake@koepcke.xyz>
2024-12-22 19:45:03 -0800

    can't remember what i did

--- notes.md
+++ notes.md
@@ -1,11 +1,4 @@
+so this needs to be a actual server so we will use node integration
 
-so this needs to be a actual server 
-so we will use node integration 
-
-index
-|
-disposable brands 
-|    |-> /[param]-[param2]
-|
-Freebase brands
-|    |-> /[freebase]
+index | disposable brands | |-> /[param]-[param2] | Freebase brands | |->
+/[freebase]
--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -1,15 +1,19 @@
-import { createDirectus, rest, } from '@directus/sdk';
+import { createDirectus, rest } from "@directus/sdk";
 
-export interface ProductEntry  {
-    id: string;
-    brand_name: string;
-    flavors: string[];
-    price: number;
-    product_image: string;
+export interface ProductEntry {
+  id: string;
+  brand_name: string;
+  flavors: string[];
+  price: number;
+  product_image: string;
+  status: boolean;
 }
 
 export const assetBaseUrl = `http://localhost:8055/assets/`;
-const directusUrl = import.meta.env.VITE_DIRECTUS_URL || "http://directus:8055";
-const directus = createDirectus(directusUrl).with(rest());
+export const directusUrl = import.meta.env.DEV
+  ? import.meta.env.VITE_DEV_URL
+  : "http://directus:8055";
 
-export default directus;
+const directusClient = createDirectus(directusUrl).with(rest());
+
+export default directusClient;
